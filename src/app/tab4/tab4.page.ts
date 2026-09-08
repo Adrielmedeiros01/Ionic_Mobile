5bfa402072681411283afcec104dddfd828e65b4
brunovsant01 <brunovsant04@gmail.com>
2026-09-08 20:23:50 -0300

    up

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { IonHeader } from "@ionic/angular/standalone";
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: './tab4.page.html',
+  styleUrls: ['./tab4.page.scss'],
+  standalone: false,
+})
+export class Tab4Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
